ee5e81a8b8909bc911efcabf8e1193ffbfe87272
vedrana <vedrana93@gmail.com>
2020-02-02 01:42:40 +0100

    fix responsive and code formatting

--- src/jquery/load-more.js
+++ src/jquery/load-more.js
@@ -1,10 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Load more button function
 
-  $("#js-button").click(function() {
+  $("#js-button").click(function () {
     $("#js-button").addClass("js-hide-btn");
     $("#js-last-element")
       .addClass("js-show-element")
       .removeAttr("id");
   });
+});
+
+$('.toggle-menu, .nav__menu--item').click(function () {
+  $(this).toggleClass('active');
+  $('#menu').toggleClass('open');
 });
